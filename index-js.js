0bd3df4da95a6432342fb3cada82940f67485ff2
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-06-05 14:11:18 -0700

    Update index-js.js

--- index-js.js
+++ index-js.js
@@ -67,12 +67,13 @@ $(function() {
   var tabs = $( "#main-panel" ).tabs();
   
 // Home tab
-  $('#btnPlayRound')            .button().click(btnPlayRoundHtml);
+$('#btnStartRound')           .button().click(btnStartRoundHtml);
+$('#btnPlayRound')            .button().click(btnPlayRoundHtml);
 
 // hi dan
 
 // Play Round tab
-  $('#btnStartRound')           .button().click(btnStartRoundHtml);
+//  $('#btnStartRound')           .button().click(btnStartRoundHtml);
   $( "#hpSelectCourse")         .change({useDefaultTee:true},  loadCourseInfo);
   $( "#hpSelectTees")           .change({useDefaultTee:false}, loadCourseInfo);
   $("#hpTargetHandicap")        .change({useDefaultTee:false}, loadCourseInfo);
