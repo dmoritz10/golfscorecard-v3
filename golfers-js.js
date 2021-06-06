8b4619419fb82ad29e799c576932ded5abb0f90e
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-06-06 10:34:19 -0700

    init

--- golfers-js.js
+++ golfers-js.js
@@ -1,81 +1,81 @@
 <script>
 
-function btnShowGolfersHtml () {
+  function btnShowGolfersHtml () {
 
-console.log("btnShowGolfersHtml")
-    
+    console.log("btnShowGolfersHtml")
+
     var golfers = readOption('Golfers', [])
-    
+
     console.log(golfers.length)
-    
+
     var x = $("#tblGolfers").clone();
     $("#glfContainer").empty()
     x.appendTo("#glfContainer");
-    
+
     $("#tblGolfers").hide()
-    
+
     $("#nbrGolfers")[0].innerHTML = golfers.length
-    
-    for (var j = 0; j<golfers.length;j++) {
-    
+
+    for (var j = 0; j<golfers.length; {
+
       var ele = $("#tblGolfers").clone();
-      
+
       var c = golfers[j].cell
-      
+
       ele.find('#glfName')[0].innerHTML = golfers[j].name
 //      ele.find('#glfCell')[0].innerHTML = c.substr(0, 3) + '-' + c.substr(3, 3) + '-' + c.substr(6,4)
-      
+
       ele.find('#btnGlfCallCourse').prop('href', 'tel:' + golfers[j].cell )
       if (!!c) {ele.find('#btnGlfCallCourse').removeClass('d-none')} else {ele.find('#btnGlfCallCourse').addClass('d-none')}
-      
+
       ele.find('#btnGlfTextTeetime').prop('href', 'sms:' + golfers[j].cell )
       if (!!c) {ele.find('#btnGlfTextTeetime').removeClass('d-none')} else {ele.find('#btnGlfTextTeetime').addClass('d-none')}
-      
+
       var tt = golfers[j]
-      
+
       var objVal = {
-            
-        idx: j,
+
+    idx: j,
         name: tt.name,
         cell: tt.cell
-        
+
       }
-      
-      var x = JSON.stringify(objVal)      
-      
+
+      var x = JSON.stringify(objVal)
+
       ele.find('#glfEditGolfer')[0].setAttribute("onclick", "editGolfer(" + x + ")");
-      
+
       ele.show()
-      
+
       ele.appendTo("#glfContainer");
-    
+
     }
-    
+
     gotoTab('Golfers')
-    
+
 console.log("btnShowGolfersHtml")
-    
+
 }
 
 async function editGolfer(objVal) {
 
     $("#golfer-modal").modal('show');
     $("#golfer-form")[0].reset();
-    
-    $('#glfmIdx').val(objVal.idx)       
-    
-    $('#glfmName').val(objVal.name)         
-    $('#glfmCell').val(objVal.cell) 
-    
+
+    $('#glfmIdx').val(objVal.idx)
+
+    $('#glfmName').val(objVal.name)
+    $('#glfmCell').val(objVal.cell)
+
     $('#btnDeleteGolfer').removeClass('d-none')
-    
+
 }
 
 async function btnAddGolferHtml () {
 
     $("#golfer-modal").modal('show');
     $("#golfer-form")[0].reset();
-    
+
     $('#btnDeleteGolfer').addClass('d-none')
 
 }
@@ -86,76 +86,76 @@ async function btnSubmitGolferHtml() {
   if (!$('#golfer-form').valid()) return
 
   var arrGolfers = readOption('Golfers', [])
-  
+
   var idx = $('#glfmIdx').val()
-  
+
   if (idx) {                                                       // update existing golfer
-    
+
     arrGolfers[idx].name = $('#glfmName').val()
     arrGolfers[idx].cell = $('#glfmCell').val()
-  
+
   } else {                                                         // add new golfer
 
     arrGolfers.push({
-      name:       $('#glfmName').val(),
-      cell:       $('#glfmCell').val()
-      
+      name: $('#glfmName').val(),
+      cell: $('#glfmCell').val()
+
     })
   }
-  
+
   arrOptions['Golfers'] = JSON.stringify(arrGolfers)
-  
+
   var signinStatus = await testAuth()
   if (!signinStatus) return
 
   await updateGolfersOption()
-  
+
   $("#golfer-modal").modal('hide');
-  
-  btnShowGolfersHtml()  
+
+  btnShowGolfersHtml()
 
 }
 
 async function btnDeleteGolferHtml() {
 
   if (arrOptions['Golfers']) {
-  
+
     var arrGolfers = readOption('Golfers')
-    
+
     var idx = $('#glfmIdx').val()
-        
+
     arrGolfers.splice(idx, 1)
-    
+
     if (arrGolfers.length == 0) arrGolfers = ''
 
   } else {
-  
+
     arrGolfers = ''
-    
+
   }
-  
+
   arrOptions['Golfers'] = arrGolfers  ? JSON.stringify(arrGolfers) : ''
-  
+
   await updateGolfersOption()
-  
+
   $("#golfer-modal").modal('hide');
-  
-  btnShowGolfersHtml()  
+
+  btnShowGolfersHtml()
 
 }
 
 async function updateGolfersOption() {
 
     var golfers = readOption('Golfers', null)
-    
+
     if (golfers) {
 
-      golfers.sort(nameCompare);
-      
+    golfers.sort(nameCompare);
+
     }
 console.time('updateOption')
   await updateOption('Golfers', golfers)
-console.timeEnd('updateOption')  
+console.timeEnd('updateOption')
 
 }
 
@@ -177,20 +177,20 @@ function nameCompare(a, b) {
 function convertGolferOption() {
 
     var golfers = readOption('Golfers', [])
-    
+
     var arr = []
-    
+
     golfers.forEach((val, idx) => {
-        
-      arr.push({name:val, cell:''})
-    
-    })
-    
+
+    arr.push({ name: val, cell: '' })
+
+  })
+
     arrOptions.Golfers = JSON.stringify(arr)
-    
+
     console.log(arrOptions.Golfers)
-    
-    updateGolfersOption() 
+
+    updateGolfersOption()
 
 }
 
