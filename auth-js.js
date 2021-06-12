cec87ff3415ceb74b76aa2b9f0f97e251539ee22
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-06-12 15:42:57 -0700

    init

---
+++ auth-js.js
@@ -0,0 +1,15 @@
+<script>
+
+function btnAuthHtml() {
+
+    gapi.auth2.getAuthInstance().signIn();
+    
+}
+    
+function btnSignoutHtml() {
+
+    // gapi.auth2.getAuthInstance().signOut();
+    
+}
+       
+</script>
