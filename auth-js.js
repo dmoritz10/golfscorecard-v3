68ca40e8ab414a10ecee6488e01d9743c6ace10c
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-06-12 16:28:31 -0700

    Update auth-js.js

--- auth-js.js
+++ auth-js.js
@@ -1,13 +1,13 @@
 <script>
 
 function btnAuthHtml(event) {
-
+alert('btnAuthHtml')
 handleAuthClick();
     
 }
     
 function btnSignoutHtml(event) {
-
+alert('btnSignoutHtml')
 handleSignoutClick();
     
 }
