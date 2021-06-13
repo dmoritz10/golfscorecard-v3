fd121737472ea8f68f9fa30b1e12515e61a4e6d8
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-06-13 16:57:25 -0700

    Update index-js.js

--- index-js.js
+++ index-js.js
@@ -1028,7 +1028,7 @@ async function btnUweatherCompHtml() {
 
   if (uweatherComp === null) return
 
-  var wPrompt = bootbox.alert({
+  var wPrompt = bootbox.prompt({
 
     title: "UWeather Station Comparison",
     message: uweatherComp,
