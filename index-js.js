9de7d082a3967cc601855dbcf60b5a279f7a65eb
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-06-14 07:20:19 -0700

    Update index-js.js

--- index-js.js
+++ index-js.js
@@ -986,7 +986,7 @@ async function checkAuth() {
   }
   var minAuthRemaining = (new Date(gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().expires_at) - new Date()) / (1000 * 60)
 
-  // alert('auth result - ' + minAuthRemaining)
+  alert('auth result - ' + minAuthRemaining)
 
 }
 
@@ -1028,9 +1028,9 @@ async function btnUweatherCompHtml() {
 
   if (uweatherComp === null) return
 
-  alert('tikes dan')
+  alert('yo dan')
 
-  var wPrompt = bootbox.dialog({
+  var wPrompt = bootbox.alert({
 
     title: "UWeather Station Comparison",
     message: uweatherComp,
