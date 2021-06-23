433389367c326dc098d78da0a36d0c5e44e8f830
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-06-23 14:19:13 -0700

    Update home-js.js

--- home-js.js
+++ home-js.js
@@ -2,6 +2,11 @@
 
 function goHome(x) {
   
+  var currHandicap = readOption('handicapObj').currHandicap
+
+  $('#hmCurrHandicap').html(currHandicap)
+  $('#hmNbrRounds').html(JSON.parse(arrOptions.Lifetime).nbrRounds)
+
   gotoTab('Home')
 
 }
