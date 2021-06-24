b22cee0a009c73e9ee97c061fe01d78170885f5e
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-06-24 06:36:52 -0700

    Update courses-js.js

--- courses-js.js
+++ courses-js.js
@@ -54,7 +54,7 @@ async function btnShowCoursesHtml () {
       ele.find('#scCourseRating')[0].innerHTML = coursesObj['USGA Course Rating']
       ele.find('#scSlopeRating')[0].innerHTML = coursesObj['Slope Rating']
       // ele.find('#scCourseHandicap')[0].innerHTML = coursesObj['Course Handicap']
-      ele.find('#scCourseHandicap')[0].innerHTML = Math.round(($('#hpSlope_Rating').html() * hcpObj.currHandicap*1) / 113)
+      ele.find('#scCourseHandicap')[0].innerHTML = Math.round((coursesObj['Slope Rating'] * hcpObj.currHandicap*1) / 113)
 
       ele.find('#scTimesPlayed')[0].innerHTML = coursesObj['Nbr Times Played'] > 0 ? coursesObj['Nbr Times Played'] : '\u00a0'
 
