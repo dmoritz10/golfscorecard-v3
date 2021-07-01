0a2982cbd4d0b18dd27c439a163116541a84d051
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-07-01 07:08:21 -0700

    Update index-js.js

--- index-js.js
+++ index-js.js
@@ -296,7 +296,7 @@ async function getSSId() {
   var q = "name = 'golfscorecard database v3'" + 
           " AND " + "mimeType='application/vnd.google-apps.spreadsheet'" + 
           " AND " + "trashed = false" + 
-          " AND " + "shared = false"
+          " AND NOT " + "sharedWithMe"
 
   var ssId = await gapi.client.drive.files.list({
       q: q,
