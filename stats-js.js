512390e91c72f355e153a1fb80facf63051bb273
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-07-04 16:13:31 -0700

    Update stats-js.js

--- stats-js.js
+++ stats-js.js
@@ -464,7 +464,7 @@ function chartPutting (title, rounds, myStatsRng, endRow)   {
       scoreCard.forEach((val, idx) => {
         nbrPutts += val.putts*1
       })
-      if (nbrHoles>0) arr.push(nbrPutts / nbrHoles) else arr.push(0)
+      if (nbrHoles>0) {arr.push(nbrPutts / nbrHoles)} else {arr.push(0)}
     })
     return arr
   }    
@@ -480,7 +480,7 @@ function chartPutting (title, rounds, myStatsRng, endRow)   {
           nbrHoles++
         }
       })
-      arr.push(nbrPutts / nbrHoles)
+      if (nbrHoles>0) {arr.push(nbrPutts / nbrHoles)} else {arr.push(0)}
     })
 console.log('arr')
 console.log(arr)
