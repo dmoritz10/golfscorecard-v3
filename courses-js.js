297d783539f9abf286ace3b98818916b4585f9eb
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-07-21 10:58:41 -0700

    Update courses-js.js

--- courses-js.js
+++ courses-js.js
@@ -495,7 +495,8 @@ function loadTeeBoxes(teeInfo){
     ele.find('#scmTeeName').eq(0).val(ti[j][tiCols.tee_name])
     ele.find('#scmGender').eq(0).val(ti[j][tiCols.gender])
     ele.find('#scmPar').eq(0).val(ti[j][tiCols.par])
-    ele.find('#scmCourseRating').eq(0).val(ti[j][tiCols.course_rating]).setAttribute("onchange", "scmCourseRatingHtml(" + j + ")");
+    ele.find('#scmCourseRating').eq(0).val(ti[j][tiCols.course_rating])
+    ele.find('#scmCourseRating').eq(0).setAttribute("onchange", "scmCourseRatingHtml(" + j + ")");
     ele.find('#scmSlopeRating').eq(0).val(ti[j][tiCols.slope_rating])
     ele.find('#scmBogeyRating').eq(0).val(ti[j][tiCols.bogey_rating])
     ele.find('#scmFront').eq(0).val(ti[j][tiCols.front])
