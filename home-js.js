bbe8bf55e83f9615ed8c83db85db58e78d42aee8
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-08-01 17:53:04 -0700

    Update home-js.js

--- home-js.js
+++ home-js.js
@@ -4,7 +4,7 @@ function goHome() {
   
   var hcpObj = readOption('handicapObj', 'hi dan')
 
-  $('#hmCurrHandicap').html(hcpObj.currHandicap).toFixed(1)
+  $('#hmCurrHandicap').html(hcpObj.currHandicap.toFixed(1))
   $('#hmNbrRounds').html(hcpObj.nbrRounds)
 
   gotoTab('Home')
