2100fe5c7e45f5ccd936e7a9211e5380575d94a0
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-08-06 15:32:01 -0700

    Update show rounds-js.js

--- show rounds-js.js
+++ show rounds-js.js
@@ -61,7 +61,7 @@ async function btnShowRoundsHtml() {
     
     var cntr = 0
     sc.scores.map((val, idx) => {if (val) {if (val.score - val.putts <= val.par - 2) cntr++;}});
-    // ele.find('#srGIR')[0].innerHTML = cntr
+    ele.find('#srGIR')[0].innerHTML = cntr
     
     var slopeRating = ci.courseInfo['Slope Rating']
     var courseRating = ci.courseInfo['USGA Course Rating']
@@ -70,7 +70,7 @@ async function btnShowRoundsHtml() {
     var hcpDiff = objHandicap.handicapDiff
     var escCorrections = objHandicap.escCorrections
 
-    ele.find('#srGIR')[0].innerHTML = escCorrections ? (hcpDiff*1).toFixed(1) + '<sup>' + escCorrections + '</sup>' : (hcpDiff*1).toFixed(1)
+    // ele.find('#srGIR')[0].innerHTML = escCorrections ? (hcpDiff*1).toFixed(1) + '<sup>' + escCorrections + '</sup>' : (hcpDiff*1).toFixed(1)
 
 
     // ele.find('#srHcpDiff')[0].innerHTML = escCorrections ? hcpDiff + '<sup>' + escCorrections + '</sup>' : hcpDiff
