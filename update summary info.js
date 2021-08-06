163484ff6bb27759b1445291d6d49d0995b41682
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-08-06 14:41:32 -0700

    Update update summary info.js

--- update summary info.js
+++ update summary info.js
@@ -1,55 +1,3 @@
-async function updateSummaryInfo() {
-
-    var hcpSelectOptions = readOption('hcpFilter')
-    var hcpMethod = hcpSelectOptions.hcpMethod
-  
-    var rounds = await getRounds()
-
-    if (!rounds) return
-
-
-    var hcpArr = []
-    var nbrRounds = 0
-    
-    for (var j = rounds.length - 1; j > -1; j--) {
-    
-      var roundObj = rounds[j]
-        
-      var sc = JSON.parse(roundObj.scoreCard)
-      var ci = JSON.parse(roundObj.courseInfo)
-      var objHandicap = roundObj.objHandicap      
-      var objTargetScore = objHandicap.targetScore
-              
-      var dtPlayed = new Date(roundObj.startTime).toString().substring(0,15)
-     
-      hcpArr.push({
-        
-        shortCourseName: shortCourseName(roundObj.courseName.toString()),
-        datePlayed: dtPlayed,
-        teePlayed: roundObj.tee,
-        score: roundObj.finalScore.toString(),
-        targetScore: objTargetScore.score,
-        courseRating: ci.courseInfo['USGA Course Rating'],
-        slopeRating: ci.courseInfo['Slope Rating'],
-        courseHandicap: objHandicap.courseHandicap,
-        hcpDiff: objHandicap.handicapDiff,
-        escCorrections: objHandicap.escCorrections,
-        rowIdx: j,
-        arrIdx: nbrRounds,
-        hcp: objHandicap.handicap
-        
-      })
-        
-      nbrRounds++
-        
-    }
-      
-  
-
-
-
-
-}
 
 function madeTargetScore(targetScore, finalScore) {
 
@@ -109,3 +57,56 @@ function calcTargetScoreDan(mostRecent20HcpDiff, targetHandicap, courseRating, s
 	}
 
 }
+
+// async function updateSummaryInfo() {
+
+//     var hcpSelectOptions = readOption('hcpFilter')
+//     // var hcpMethod = hcpSelectOptions.hcpMethod
+  
+//     var rounds = await getRounds()
+
+//     if (!rounds) return
+
+
+//     var hcpArr = []
+//     var nbrRounds = 0
+    
+//     for (var j = rounds.length - 1; j > -1; j--) {
+    
+//       var roundObj = rounds[j]
+        
+//       // var sc = JSON.parse(roundObj.scoreCard)
+//       var ci = JSON.parse(roundObj.courseInfo)
+//       var objHandicap = roundObj.objHandicap      
+//       var objTargetScore = objHandicap.targetScore
+              
+//       var dtPlayed = new Date(roundObj.startTime).toString().substring(0,15)
+     
+//       hcpArr.push({
+        
+//         shortCourseName: shortCourseName(roundObj.courseName.toString()),
+//         datePlayed: dtPlayed,
+//         teePlayed: roundObj.tee,
+//         score: roundObj.finalScore.toString(),
+//         targetScore: objTargetScore.score,
+//         courseRating: ci.courseInfo['USGA Course Rating'],
+//         slopeRating: ci.courseInfo['Slope Rating'],
+//         courseHandicap: objHandicap.courseHandicap,
+//         hcpDiff: objHandicap.handicapDiff,
+//         escCorrections: objHandicap.escCorrections,
+//         rowIdx: j,
+//         arrIdx: nbrRounds,
+//         hcp: objHandicap.handicap
+        
+//       })
+        
+//       nbrRounds++
+        
+//     }
+      
+  
+
+
+
+
+// }
