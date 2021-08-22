812380e0bd26711a073e94a64b446554f60ca234
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-08-22 15:27:08 -0700

    Update scorecard-js.js

--- scorecard-js.js
+++ scorecard-js.js
@@ -1404,14 +1404,14 @@ function btnGolfersHtml() {
   var inputOptions = getGolfers()
 
   console.log(inputOptions)
-  console.log(prScore.golfers.map(a => JSON.stringify(a)))
+  console.log(prScore.golfers.map(a => a.name))
 
   if (inputOptions.length == 0) return
   
   var golferPrompt = bootbox.prompt({
     
     title: "Select Golfers",
-    value: prScore.golfers ? prScore.golfers.map(a => JSON.stringify(a)) : [],
+    value: prScore.golfers ? prScore.golfers.map(a => a.name) : [],
     inputType: 'checkbox',
     className: 'golfersCSS',
     closeButton: false,
