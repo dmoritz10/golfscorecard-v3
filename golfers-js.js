4868d8c02faf914b805291cb41014b62a7513fd4
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-09-08 11:39:20 -0700

    Update golfers-js.js

--- golfers-js.js
+++ golfers-js.js
@@ -99,6 +99,7 @@ console.log('dan')
 
 console.log(arrGolfers)
 console.log($('#glfmName').val())
+console.log(arrGolfers.indexOf($('#glfmName').val())
 
     if (arrGolfers.indexOf($('#glfmName').val()) > -1) {
       toast("Golfer already exists")
