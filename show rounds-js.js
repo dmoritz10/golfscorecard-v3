6df4ab424d4e57f34fa3043d0d6a7fbddc464af9
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-09-10 16:02:22 -0700

    Update show rounds-js.js

--- show rounds-js.js
+++ show rounds-js.js
@@ -12,6 +12,8 @@ async function btnShowRoundsHtml() {
   
   var rounds = await getRounds(srExcludeSmall, srSelectedCourse)
 
+  rounds.sort((a, b) => b.objHandicap.handicapDiff - a.objHandicap.handicapDiff);
+
   if (!rounds) return
 
 
@@ -61,7 +63,7 @@ async function btnShowRoundsHtml() {
     
     var cntr = 0
     sc.scores.map((val, idx) => {if (val) {if (val.score - val.putts <= val.par - 2) cntr++;}});
-    ele.find('#srGIR')[0].innerHTML = cntr
+    // ele.find('#srGIR')[0].innerHTML = cntr
     
     var slopeRating = ci.courseInfo['Slope Rating']
     var courseRating = ci.courseInfo['USGA Course Rating']
@@ -70,7 +72,7 @@ async function btnShowRoundsHtml() {
     var hcpDiff = objHandicap.handicapDiff ? (objHandicap.handicapDiff*1).toFixed(1) : '&nbsp;'
     var escCorrections = objHandicap.escCorrections
 
-    // ele.find('#srGIR')[0].innerHTML = escCorrections ? (hcpDiff*1).toFixed(1) + '<sup>' + escCorrections + '</sup>' : (hcpDiff*1).toFixed(1)
+    ele.find('#srGIR')[0].innerHTML = escCorrections ? (hcpDiff*1).toFixed(1) + '<sup>' + escCorrections + '</sup>' : (hcpDiff*1).toFixed(1)
 
 
     // ele.find('#srHcpDiff')[0].innerHTML = escCorrections ? hcpDiff + '<sup>' + escCorrections + '</sup>' : hcpDiff
