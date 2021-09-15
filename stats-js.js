6461740a823c05c13b4a6f3c82881ca61af680c4
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-09-15 08:47:45 -0700

    Update stats-js.js

--- stats-js.js
+++ stats-js.js
@@ -850,7 +850,7 @@ function chartTeeToGreen          (title, rounds, myStatsRng, endRow) {
     if (rnds.length == 0 ) var rnds = extrRndData	(rounds, null, endRow.row2)
     if (rnds.length == 0 ) var rnds = extrRndData	(rounds, null, endRow.row1)
 
-    var endRowGraphs = Math.max(endRow.row1, endRow.row2, endRow.row3)
+    var endRowGraphs = Math.min(endRow.row1, endRow.row2, endRow.row3)
     var rnds = extrRndData	(rounds, null, endRowGraphs)
 
     var chart = graphTeeToGreen(rnds)
