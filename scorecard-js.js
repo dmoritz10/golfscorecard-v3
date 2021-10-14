a9dce78e8d8d5e6ed665d5f51bec9b2be9e79669
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-10-14 06:44:42 -0700

    Update scorecard-js.js

--- scorecard-js.js
+++ scorecard-js.js
@@ -424,7 +424,7 @@ async function getHoleDetail(sxsCourseId, tee, gender) {
     
 //    request.open('GET', 'https://cors-anywhere.herokuapp.com/' + sxsCourseId)
     // request.open('GET', 'https://cors.bridged.cc/' + sxsCourseId)
-    request.open('GET', 'https://yacdn.org/serve/' + sxsCourseId)
+    request.open('GET', 'https://test.cors.workers.dev/?' + sxsCourseId)
 
     
 
