f20c047ff735caf114656fbedabae63d40458ca8
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2021-11-10 15:50:17 -0800

    dan

--- index-js.js
+++ index-js.js
@@ -927,8 +927,8 @@ function parseWeatherText(wRptHtml) {
   if (strToken < 0) 
     var strToken = '</a></div></div><p _ngcontent-sc248="">'
 
-  var str = wRptHtml.match(/(?<=>).*?(?=</p></div></div></div>)/)    
-
+  var str = wRptHtml.match(/(?<=>).*?(?=<\/p><\/div><\/div><\/div>)/)   
+  
   console.log('str')
   console.log(str)
 
